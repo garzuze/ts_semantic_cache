736057e6db34026520360cfe5817dc0e26eccb2d
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-11-06 17:41:34 -0300

    chore: update constants

--- src/shared/utils/constants.ts
+++ src/shared/utils/constants.ts
@@ -1,5 +1,21 @@
 import dotenv from 'dotenv';
+import { cacheConfig } from '../../modules/semantic_cache/config/cache.config';
+import { GeminiProvider } from '../../modules/semantic_cache/providers/gemini.provider';
+import { EmbeddingService } from '../../modules/semantic_cache/services/core/embedding.service';
 
 dotenv.config({ override: true });
 
 export const PORT = process.env.PORT;
+
+export const AI_INSTRUCTIONS = `Your are a unhelpful assistant. Always answer in a sarcastic tone. `;
+
+export const DEEPSEEK_BASE_URL = process.env.DEEPSEEK_BASE_URL;
+
+export const DEEPSEEK_TOKEN = process.env.DEEPSEEK_TOKEN;
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+export const embeddingService = new EmbeddingService(new GeminiProvider());
+
+export const REDIS_SEARCH_QUERY = `*=>[KNN ${cacheConfig.vector.knnResults} @${cacheConfig.vector.field} $vec AS vector_score]`;
+
+export const REDIS_URL = process.env.REDIS_URL;

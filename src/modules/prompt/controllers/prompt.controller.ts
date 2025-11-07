9076aa6f9fb473f37454036f851455e725b45992
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-11-07 17:26:11 -0300

    feat: create api factory

--- src/modules/prompt/controllers/prompt.controller.ts
+++ src/modules/prompt/controllers/prompt.controller.ts
@@ -1,10 +1,11 @@
 import type { Request, Response } from 'express';
-import api from '../../../shared/services/api';
 import { SemanticCacheService } from '../../semantic_cache/services/core/semantic-cache.service';
+import { createApi } from '../factories/api.factory';
 import { PromptResponse } from '../interfaces/prompt-response.interface';
 import { DeepSeekPromptService } from '../services/deepseek-prompt.service';
 
 const semanticCache = new SemanticCacheService();
+const api = createApi('deepseek');
 const promptService = new DeepSeekPromptService(api, semanticCache);
 
 export const createPrompt = async (
---
+++ src/modules/prompt/factories/api.factory.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosInstance } from 'axios';
+import { llmProviders } from '../config/llm-providers.config';
+import { LlmProvider } from '../interfaces/llm-provider.interface';
+
+export const createApi = (provider: LlmProvider): AxiosInstance => {
+  const { baseURL, headers, token } = llmProviders[provider];
+  const api = axios.create({
+    baseURL,
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+  });
+
+  api.interceptors.request.use((request) => {
+    if (token) request.headers.Authorization = `Bearer ${token}`;
+    return request;
+  });
+
+  api.interceptors.response.use(
+    function onFulfilled(response) {
+      return response;
+    },
+    function onRejected(error) {
+      return Promise.reject(error);
+    },
+  );
+
+  return api;
+};
